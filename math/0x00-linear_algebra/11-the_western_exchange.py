65685a6207c6cd45e6c6a6971309270d29173fd6
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-17 03:12:27 +0000

    Uploading task 11 py function to transpose matrices

---
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task 11 function to transpose ndarray"""
+import numpy as np
+
+
+def np_transpose(matrix):
+    """transpose given matrix"""
+    return np.transpose(matrix)
