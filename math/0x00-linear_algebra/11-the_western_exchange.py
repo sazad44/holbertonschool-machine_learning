bffed2ada40388a73af202e812c6947eae87c310
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-17 03:16:19 +0000

    Updating task 11 py func to not import numpy

--- math/0x00-linear_algebra/11-the_western_exchange.py
+++ math/0x00-linear_algebra/11-the_western_exchange.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """task 11 function to transpose ndarray"""
-import numpy as np
 
 
 def np_transpose(matrix):
     """transpose given matrix"""
-    return np.transpose(matrix)
+    return matrix.transpose()
