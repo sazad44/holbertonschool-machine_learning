fb945f8930bf5b1b3e1908d453ff4236c9f453eb
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-17 03:33:28 +0000

    Uploading task 14 function to multiply matrices

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""task 14 function to multiply two given matrices"""
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """multiply two given matrices"""
+    return np.matmul(mat1, mat2)
