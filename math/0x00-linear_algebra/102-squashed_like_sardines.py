4752cfa47cc295de5148942fc89896e564d70a08
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-20 01:51:00 +0000

    Updating mismatch detectin of cat matrix function

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -4,7 +4,7 @@
 
 def cat_matrices(mat1, mat2, axis=0):
     """py functin to concatenate matrices on a specified axis"""
-    if not isinstance(mat1[0], type(mat2[0])):
+    if (axis % 2) and len(mat1) != len(mat2):
         return None
     curMat1 = mat1
     curMat2 = mat2
@@ -15,8 +15,6 @@ def cat_matrices(mat1, mat2, axis=0):
         curMat2 = curMat2[0]
     newMat = []
     if axis > 0:
-        if len(mat1) != len(mat2):
-            return None
         for i in range(len(mat1)):
             newMat.append(cat_matrices(mat1[i], mat2[i], axis - 1))
     else:
