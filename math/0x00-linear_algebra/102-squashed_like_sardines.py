d6877161c2e7550b697f66f666b42ff52fccf2a1
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-20 02:40:00 +0000

    Updating task 17 py functin with better check for mismatch

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -9,11 +9,19 @@ def cat_matrices(mat1, mat2, axis=0):
     curMat1 = mat1
     curMat2 = mat2
     while not isinstance(curMat1, int):
-        if not isinstance(curMat2[0], type(curMat1[0])):
+        """if not isinstance(curMat2[0], type(curMat1[0])):
             return None
         elif isinstance(curMat1[0], int) and len(curMat1) != len(curMat2):
             if axis % 2 != 0:
+                return None"""
+        if isinstance(curMat1[0], int) and len(curMat1) != len(curMat2):
+            if axis % 2:
                 return None
+        elif isinstance(curMat1[0], list) and isinstance(curMat1[0][0], int):
+            if len(curMat1) != len(curMat2) and not axis % 2:
+                return None
+        elif not isinstance(curMat2[0], type(curMat1[0])):
+            return None
         curMat1 = curMat1[0]
         curMat2 = curMat2[0]
     newMat = []
