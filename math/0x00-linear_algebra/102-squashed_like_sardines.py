20272cdbadaa8f9caf3388c5f5592bd063f7df8c
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-20 01:44:04 +0000

    Updating function to handle mismatch cases early

--- math/0x00-linear_algebra/102-squashed_like_sardines.py
+++ math/0x00-linear_algebra/102-squashed_like_sardines.py
@@ -6,18 +6,18 @@ def cat_matrices(mat1, mat2, axis=0):
     """py functin to concatenate matrices on a specified axis"""
     if not isinstance(mat1[0], type(mat2[0])):
         return None
+    curMat1 = mat1
+    curMat2 = mat2
+    while not isinstance(curMat1, int):
+        if not isinstance(curMat2[0], type(curMat1[0])):
+            return None
+        curMat1 = curMat1[0]
+        curMat2 = curMat2[0]
     newMat = []
     if axis > 0:
         for i in range(len(mat1)):
             newMat.append(cat_matrices(mat1[i], mat2[i], axis - 1))
     else:
-        curMat1 = mat1
-        curMat2 = mat2
-        while not isinstance(curMat1, int):
-            if not isinstance(curMat2[0], type(curMat1[0])):
-                return None
-            curMat1 = curMat1[0]
-            curMat2 = curMat2[0]
         for n in mat1:
             newMat.append(n)
         for n in mat2:
