8ff83ee3098b85e7751f6d87f34b30ce3d1955ad
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-26 22:19:20 +0000

    uploading function to integrate polynomial task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+"""task 17 py function to integrate a polynomial"""
+
+
+def poly_integral(poly, C=0):
+    """py function to calculate integral of poly"""
+    if not isinstance(
+            poly,
+            list
+    ) or len(
+        poly
+    ) == 0 or not isinstance(
+            C, (
+                float,
+                int
+            )
+    ):
+        return None
+    newPoly = [C]
+    for c in range(len(poly)):
+        appVal = poly[c] / (c + 1)
+        if appVal.is_integer():
+            newPoly.append(int(appVal))
+        else:
+            newPoly.append(appVal)
+    return newPoly
