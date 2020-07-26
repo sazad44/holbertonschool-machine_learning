312f05451987c78ae58b7c201c39cddfeacea510
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-26 21:29:30 +0000

    Updating task 9 to only accept integers 1 and greater

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,6 +4,6 @@
 
 def summation_i_squared(n):
     """calculates summ of i(squared) from 1 to n"""
-    if not isinstance(n, int):
+    if not isinstance(n, int) or n < 1:
         return None
     return int(n * (n + 1) * (2 * n + 1) / 6)
