f45bdd2c630831a3904816ffaa7d166cc3117e40
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-30 22:48:09 +0000

    Uploading task 0 poisson dist repr class

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Poisson Class to represent poisson distribution"""
+
+
+class Poisson():
+    """Representation of Poisson Distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        """initialization function for class"""
+        if data is None:
+            if lambtha <= 0:
+                raise ValueError("lambtha must be a positive value")
+            self.lambtha = lambtha
+        else:
+            if not isinstance(data, list):
+                raise ValueError("data must be a list")
+            elif len(data) < 2:
+                raise ValueError("data must contain multiple values")
+            else:
+                self.lambtha = sum(data) / len(data)
