2e48dfc352553f56486edff1aa2fc528bdf2b9f0
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-30 23:08:55 +0000

    Updating poisson dist repr with pmf func

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -6,6 +6,8 @@ class Poisson():
     """Representation of Poisson Distribution"""
     def __init__(self, data=None, lambtha=1.):
         """initialization function for class"""
+        self.e = 2.7182818285
+        self.pi = 3.1415926536
         if data is None:
             if lambtha <= 0:
                 raise ValueError("lambtha must be a positive value")
@@ -18,3 +20,15 @@ class Poisson():
                 raise ValueError("data must contain multiple values")
             else:
                 self.lambtha = sum(data) / len(data)
+
+    def pmf(self, k):
+        """calculates value of PMF for given successes"""
+        if not isinstance(k, int):
+            k = int(k)
+        denom = 1
+        for i in range(k, 1, -1):
+            denom *= i
+        pmfValue = ((self.e**(-self.lambtha))*(self.lambtha**k)) / denom
+        if pmfValue < 0 or pmfValue > 1:
+            return 0
+        return pmfValue
