283b88e54bfbb9c840447ce6428386dc8d9d79d7
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-07-31 06:09:04 +0000

    Updating binomial class with pmf func

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -25,6 +25,35 @@ class Binomial():
             for d in data:
                 variance += (mean - d) ** 2
             variance = variance / len(data)
-            self.p = -((variance / mean) - 1)
-            self.n = int(mean / self.p)
+            self.p = 1 - (variance / mean)
+            self.n = int(round(mean / self.p))
             self.p = float(mean / self.n)
+
+    def pmf(self, k):
+        """Calculate pmf of binomial given k successes"""
+        if not isinstance(k, int):
+            k = int(k)
+        if k < 0:
+            return 0
+        nFac = 1
+        kFac = 1
+        nMkFac = 1
+        for i in range(1, self.n + 1):
+            nFac *= i
+        for i in range(1, k + 1):
+            kFac *= i
+        for i in range(1, self.n - k + 1):
+            nMkFac *= i
+        return (
+            nFac / (
+                kFac * nMkFac
+            )
+        ) * (
+            self.p ** k
+        ) * (
+            (
+                1 - self.p
+            ) ** (
+                self.n - k
+            )
+        )
