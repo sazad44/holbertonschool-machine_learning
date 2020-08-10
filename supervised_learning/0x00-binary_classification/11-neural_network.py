00d8f3bd29bd5f032482966beed41b1de2b150e2
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-08-10 05:41:06 +0000

    Uploading cost func update to NN class

---
+++ supervised_learning/0x00-binary_classification/11-neural_network.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+"""Class definitions for neural network with 1 hidden layer"""
+import numpy as np
+
+
+class NeuralNetwork():
+    """Neural network class definition"""
+    def __init__(self, nx, nodes):
+        """initialization func for class"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        if not isinstance(nodes, int):
+            raise TypeError("nodes must be an integer")
+        elif nodes < 1:
+            raise ValueError("nodes must be a positive integer")
+        self.__W1 = np.random.randn(nodes, nx)
+        self.__b1 = np.array([float(0)] * nodes).reshape(nodes, 1)
+        self.__A1 = 0
+        self.__W2 = np.random.randn(1, nodes)
+        self.__b2 = 0
+        self.__A2 = 0
+
+    @property
+    def W1(self):
+        """getter func for W1"""
+        return self.__W1
+
+    @property
+    def b1(self):
+        """getter func for b1"""
+        return self.__b1
+
+    @property
+    def A1(self):
+        """getter func for A1"""
+        return self.__A1
+
+    @property
+    def W2(self):
+        """getter func for W2"""
+        return self.__W2
+
+    @property
+    def b2(self):
+        """getter func for b2"""
+        return self.__b2
+
+    @property
+    def A2(self):
+        """getter func for A2"""
+        return self.__A2
+
+    def forward_prop(self, X):
+        """calculates forward propagation of neural network"""
+        matmul = np.matmul(self.__W1, X)
+        actVals = 1 / (1 + np.exp(-(matmul + self.__b1)))
+        self.__A1 = actVals
+        matmul = np.matmul(self.__W2, self.__A1)
+        actVals = 1 / (1 + np.exp(-(matmul + self.__b2)))
+        self.__A2 = actVals
+        return self.__A1, self.__A2
+
+    def cost(self, Y, A):
+        """calculates cost of the model using logistic regression"""
+        costMat = -(Y * (np.log(A)) + (1 - Y) * np.log(1.0000001 - A))
+        costVal = np.sum(costMat) / len(costMat[0])
+        return costVal
