4b3c3e215cd319128751d2a92231ce552c9bbbe5
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-08-10 00:03:16 +0000

    Uploading Neuron class py file

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""class for Neuron"""
+import numpy as np
+
+
+class Neuron():
+    """Neuron Class to define a single neuron performing binary classification"""
+    def __init__(self, nx):
+        """initialization function for class"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.W = np.random.randn(1, nx)
+        self.b = 0
+        self.A = 0
