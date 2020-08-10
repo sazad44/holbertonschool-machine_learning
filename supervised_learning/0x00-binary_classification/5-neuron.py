9e91f87c61d8976525ac253901fc16015b36b7dc
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-08-10 03:23:10 +0000

    Uploading task 5 neuron class with GD func

---
+++ supervised_learning/0x00-binary_classification/5-neuron.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python3
+"""class for Neuron"""
+import numpy as np
+
+
+class Neuron():
+    """Neuron Class to define a single neuron performing classification"""
+    def __init__(self, nx):
+        """initialization function for class"""
+        if not isinstance(nx, int):
+            raise TypeError("nx must be an integer")
+        elif nx < 1:
+            raise ValueError("nx must be a positive integer")
+        self.__W = np.random.randn(1, nx)
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        """getter func for W"""
+        return self.__W
+
+    @property
+    def b(self):
+        """getter func for b"""
+        return self.__b
+
+    @property
+    def A(self):
+        """getter func for A"""
+        return self.__A
+
+    def forward_prop(self, X):
+        """forward propagation func for Neuron"""
+        matmul = np.matmul(self.__W, X)
+        actVals = 1 / (1 + np.exp(-(matmul + self.__b)))
+        self.__A = actVals
+        return self.__A
+
+    def cost(self, Y, A):
+        """calculates the cost of the model using logistic regression"""
+        costMat = -(Y * (np.log(A)) + (1 - Y) * np.log(1.0000001 - A))
+        costSum = sum(costMat[0])
+        costVal = costSum / len(costMat[0])
+        return costVal
+
+    def evaluate(self, X, Y):
+        """evaluates neuron's predictions"""
+        A = self.forward_prop(X)
+        C = self.cost(Y, A)
+        return np.round(A).astype(int), C
+
+    def gradient_descent(self, X, Y, A, alpha=0.05):
+        """calculates one pass of gradient descent on the neuron"""
+        dz = (A - Y).reshape(len(A[0]), 1)
+        xdz = np.matmul(X, dz).reshape(1, len(self.__W[0]))
+        self.__W -= alpha*xdz
+        db = alpha * (A - Y)
+        dbAvg = sum(db[0]) / len(db[0])
+        self.__b -= dbAvg
