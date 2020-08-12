b92e89b980a8f1360a24f4ed5654a2ab0dfac679
Samie Azad <36613205+sazad44@users.noreply.github.com>
2020-08-12 05:25:06 +0000

    Updating edge case handling

--- supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -5,9 +5,8 @@ import numpy as np
 
 def one_hot_encode(Y, classes):
     """encodes label vector to one hot matrix"""
-    try:
-        oh = np.zeros((classes, Y.max()+1))
-        oh[Y, np.arange(Y.size)] = 1
-        return oh
-    except:
+    if not isinstance(Y[0], int):
         return None
+    oh = np.zeros((classes, Y.max()+1))
+    oh[Y, np.arange(Y.size)] = 1
+    return oh
